b970b9c5b2c789d8841fafc3eff6d96396c2ca29
Lasse Westfall Bruun <lasseb200@gmail.com>
2026-05-28 23:01:02 +0200

    v1.0 MT

--- KernelSVM/private/KernelSVM_wconf_Platt.m
+++
@@ -1,4 +0,0 @@
-function [Y_pred, confidence] = KernelSVM_wconf_Platt(X_training,Y_training,X_test,type,C,input)
-    [alpha, b, kernel, Yu, Xr_cell, yr_cell,plattA,plattB] = KernelSVM_training_Platt(X_training,Y_training,type,C,input);
-    [Y_pred, confidence] = KernelSVM_1v1classifier_wconf_Platt(X_test,alpha,b,kernel,Yu,Xr_cell,yr_cell,plattA,plattB);
-end
